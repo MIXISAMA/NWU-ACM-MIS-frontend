2d5aad42a59823847dbd0bfce7167c52382ec9d0
PeiGiZhu <ZhuXuqi200061@163.com>
2021-04-15 00:37:16 +0800

    增加统计视图
    添加统计视图基本模块
    添加子路由

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -6,10 +6,10 @@
   <el-col :xs="13" :sm="11" :lg="8">
     <el-menu default-active="/" class="el-menu-demo" mode="horizontal" @select="handleSelect" :router="true">
       <el-menu-item index="/">主页</el-menu-item>
-      <el-menu-item index="publicity">宣传</el-menu-item>
-      <el-menu-item index="oj" disabled>oj</el-menu-item>
-      <el-menu-item index="team">组队</el-menu-item>
-      <el-menu-item index="statistics">统计</el-menu-item>
+      <el-menu-item index="/publicity">宣传</el-menu-item>
+      <el-menu-item index="/oj" disabled>oj</el-menu-item>
+      <el-menu-item index="/team">组队</el-menu-item>
+      <el-menu-item index="/statistics">统计</el-menu-item>
     </el-menu>
   </el-col>
   <el-col :xs="1" :sm="1" :lg="1">
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,3 +1,74 @@
 <template>
-  统计
+  <el-container>
+    <el-aside>
+      <el-col :span="12">
+        <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
+          style="width:200px;" >
+          <el-submenu index="1">
+            <template #title>
+              <i class="el-icon-location"></i>
+              <span>战力榜</span>
+            </template>
+            <router-link to="/statistics/solve-problems" class="router-link-activate">
+            <el-menu-item>
+              <template #title>
+                <i class="el-icon-s-data"></i>
+                  <span><b>刷题统计</b></span>
+              </template>
+            </el-menu-item>
+            </router-link>
+            <router-link to="/statistics/rating" class="router-link-activate">
+            <el-menu-item>
+              <template #title>
+                <i class="el-icon-s-marketing"></i>
+                  <span><b>Rating</b></span>
+              </template>
+            </el-menu-item>
+            </router-link>
+          </el-submenu>
+          <router-link to="/statistics/date-arrange" class="router-link-activate">
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <template #title>
+              训练日程
+            </template>
+          </el-menu-item>
+          </router-link>
+          <router-link to="/statistics/contribution" class="router-link-activate">
+          <el-menu-item index="3">
+            <i class="el-icon-star-off"></i>
+            <template #title>
+              贡献点
+            </template>
+          </el-menu-item>
+          </router-link>
+        </el-menu>
+      </el-col>
+    </el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
+
+<script lang="ts">
+  export default {
+    methods: {
+      handleOpen(key: string, keyPath: string[]): void {
+        console.log("open", key, keyPath);
+      },
+      handleClose(key: string, keyPath: string[]): void {
+        console.log("close", key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.router-link-activate{
+  text-decoration: none;
+}
+</style>
