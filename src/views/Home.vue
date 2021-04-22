6d7d099b58545eec909505b56ce3a9f8028fdd6d
NwuLibm <libmght@foxmail.com>
2021-04-22 11:42:31 +0800

    修改homepage
    修改homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,28 @@
 <template>
-  <div style="width:1300px; margin:0 auto;">
-    <el-carousel :interval="4000" type="card" height="250px">
+  <p class="p">Welcome to Nwu Acm!</p> 
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br >
+  <br>
+  <br>
+  <div style="width:1600px; margin:0 auto;">
+    <el-carousel :interval="4000" type="card" width="800px" height="300px">
       <el-carousel-item v-for="item in 4" :key="item">
         <h3 class="medium">
           <el-image
-            style="width: 680px; height: 250px"
+            style="width: 800px; height: 300px;"
             :src="url[item - 1]"
             :fit="fit"></el-image>
         </h3>
@@ -29,11 +47,16 @@
 </script>
 
 <style>
+  .p{
+    font-size: 50px;
+    text-align:center;
+    font-weight: bold;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  }
   .el-carousel__item h3 {
     color: #475669;
     font-size: 14px;
-    opacity: 0.75;
-    line-height: 250px; 
+    opacity: 0.85;
     margin: 0;
   }
 
