02e470693c51f531fb2c08138b02419dfe1c5681
PeiGiZhu <ZhuXuqi200061@163.com>
2021-05-04 19:50:56 +0800

    完善统计模块
    增加persondetail 内容
    (*侧边滚动条问题未解决)
    将“组队”模块替换成“训练赛”

--- src/views/Statistics/PersonDetail.vue
+++ src/views/Statistics/PersonDetail.vue
@@ -2,6 +2,35 @@
   <router-link to="../person-data">
     <el-button type="botton" class="button">返回</el-button>
   </router-link>
+    <!-- <el-scrollbar > -->
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <div>
+          <el-avatar shape="square" :size="200" fit="fill"
+            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg">
+          </el-avatar>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div>
+          姓名:
+        </div>
+        <br />
+        <div>
+          学号:
+        </div>
+        <br />
+        <div>
+          专业:
+        </div>
+        <br />
+      </el-col>
+    </el-row>
+    <br /><br />
+    <el-row>
+      <v-chart class="chart" :option="option" />
+    </el-row>
+  <!-- </el-scrollbar> -->
 </template>
 
 <style scoped>
@@ -11,14 +40,96 @@
     left: 90%;
     top: 15%
   }
+
+  .el-scrollbar__thumb {
+    display: none;
+  }
+
+  .el-scrollbar__wrap {
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+
+  .chart {
+    height: 400px;
+    width: 1000px;
+    position: absolute;
+    left: 0%;
+    top: 20%;
+  }
 </style>
 
 <script>
-  export default {
+  import {
+    use
+  } from "echarts/core";
+  import {
+    GridComponent,
+    LegendComponent,
+    TitleComponent,
+  } from 'echarts/components';
+  import {
+    LineChart
+  } from 'echarts/charts';
+  import {
+    CanvasRenderer
+  } from 'echarts/renderers';
+
+  import VChart, {
+    THEME_KEY
+  } from "vue-echarts";
+  import {
+    ref,
+    defineComponent
+  } from "vue";
+
+  use([
+    GridComponent,
+    LineChart,
+    LegendComponent,
+    CanvasRenderer,
+    TitleComponent,
+  ]);
+
+  export default defineComponent({
     methods: {
       goBack() {
         console.log('go back');
       }
+    },
+    components: {
+      VChart
+    },
+    provide: {
+      [THEME_KEY]: "light"
+    },
+    setup: () => {
+      const option = ref({
+        title: {
+          text: 'Rating Change',
+          left: '5%',
+          top: '0%',
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: "{b} : {c}"
+        },
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [1250, 1230, 1224, 1200, 1350, 1470, 1360],
+          type: 'line'
+        }]
+      });
+
+      return {
+        option
+      };
     }
-  }
+  });
 </script>
