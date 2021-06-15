c77d2a73596c7d8e89ffa53bb28dee45a734e433
NwuLibm <libmght@foxmail.com>
2021-06-15 19:20:41 +0800

    homepage-zmd
    添加走马灯（轮播）

--- src/components/HomeSectionFrontCover.vue
+++ src/components/HomeSectionFrontCover.vue
@@ -1,11 +1,63 @@
 <template>
-  <section>
-    封面
+  <section style="height: 100%;">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+          aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+          aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+          aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active zmd" v-bind:style="{backgroundImage: 'url(https://mail.nwu.edu.cn/coremail/common/assets/index_cmxt50/img/mainBg1.jpg)'}">
+          <p class="text-shadow">Welcome to NWU ACM!</p>
+          <a href="#introduction"> <button type="button" class="btn btn-dark btn-lg about">Get Start</button></a>
+        </div>
+        <div class="carousel-item zmd" v-bind:style="{backgroundImage: 'url(https://mail.nwu.edu.cn/coremail/common/assets/index_cmxt50/img/mainBg2.jpg)'}">
+          
+        </div>
+        <div class="carousel-item zmd" v-bind:style="{backgroundImage: 'url(https://mail.nwu.edu.cn/coremail/common/assets/index_cmxt50/img/mainBg3.jpg)'}">
+
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
   </section>
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-class-component'
+  import {
+    Vue
+  } from 'vue-class-component'
 
-export default class HomeSectionFrontCover extends Vue {}
+  export default class HomeSectionFrontCover extends Vue {}
 </script>
+
+<style scoped>
+ .text-shadow{
+    text-shadow:#13121a 0 0 10px;
+    color:rgb(231, 221, 221);
+    font-size:60px;
+    float: left;
+  }
+  .about {
+    margin-top: 1%;
+    margin-left: 3%;
+  }
+  .zmd{
+    background-size:100% 100%;
+    height: 100vh;
+    width: 100vw;
+    opacity: 0.8;
+  }
+</style>
--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="background">
     Footer
   </div>
 </template>
